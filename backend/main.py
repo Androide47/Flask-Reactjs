270f96f902e4027cf35f2ee68fbf95aef29561c8
Andrei Ortega <75287088+Androide47@users.noreply.github.com>
2024-06-23 22:01:58 -0600

    first commit

---
+++ backend/main.py
@@ -0,0 +1,32 @@
+from flask import request, jsonify
+from config import app, db
+from models import Contact
+
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    constacts = Contact.query.all()
+    jason_contacts = list(map(lambda x: x.to_json(), constacts))
+    return jsonify({"contacts": jason_contacts})
+
+@app.route("/create_contact", methods=["POST"])
+def create_contact():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+
+    if not  first_name or not last_name or not email:
+        return jsonify({"message": "You must provide first name, last name and email"}), 400
+
+    contact = Contact(first_name=first_name, last_name=last_name, email=email)
+
+    try:    
+        db.session.add(contact)
+        db.session.commit()
+    except:     
+        return jsonify({"message": "Contact created"}), 201
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
